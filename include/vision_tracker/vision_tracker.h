202af7bc6c7cb169c31bc7d8ef0a18f7585b8700
MasayaKataoka <ms.kataoka@gmail.com>
2019-04-29 20:06:39 +0900

    add message filters

--- include/vision_tracker/vision_tracker.h
+++ include/vision_tracker/vision_tracker.h
@@ -1,5 +1,5 @@
-#ifndef VISION_TRACKER_H_INCLUDED
-#define VISION_TRACKER_H_INCLUDED
+#ifndef VISION_TRACKER_VISION_TRACKER_H_INCLUDED
+#define VISION_TRACKER_VISION_TRACKER_H_INCLUDED
 
 // Headers in OpenCV
 #include <opencv2/core.hpp>
@@ -11,24 +11,32 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <image_transport/subscriber_filter.h>
+#include <message_filters/subscriber.h>
 #include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
+#include <sensor_msgs/Image.h>
+#include <vision_msgs/Detection2DArray.h>
 
 // headers in STL
 #include <memory>
 
 namespace vision_tracker
 {
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image,vision_msgs::Detection2DArray> SyncPolicy;
+
     class VisionTracker
     {
     public:
         VisionTracker(ros::NodeHandle nh,ros::NodeHandle pnh);
         ~VisionTracker();
     private:
+        void detectionCallback(const sensor_msgs::ImageConstPtr& image_ptr,const vision_msgs::Detection2DArray::ConstPtr& detection_ptr);
         std::shared_ptr<image_transport::SubscriberFilter> image_subscriber_ptr_;
+        std::shared_ptr<message_filters::Subscriber<vision_msgs::Detection2DArray> > detection_sub_ptr__;
+        std::shared_ptr<message_filters::Synchronizer<SyncPolicy> > sync_ptr_;
         ros::NodeHandle nh_;
         ros::NodeHandle pnh_;
         image_transport::ImageTransport it_;
     };
 }
-#endif  //VISION_TRACKER_H_INCLUDED
+#endif  //VISION_TRACKER_VISION_TRACKER_H_INCLUDED
--- src/vision_tracker.cpp
+++ src/vision_tracker.cpp
@@ -7,10 +7,18 @@ namespace vision_tracker
         nh_(nh),pnh_(pnh),it_(nh)
     {
         image_subscriber_ptr_ = std::make_shared<image_transport::SubscriberFilter>(it_,"image_raw", 1);
+        detection_sub_ptr__ = std::make_shared<message_filters::Subscriber<vision_msgs::Detection2DArray> >(nh_,"detection",1);
+        sync_ptr_ = std::make_shared<message_filters::Synchronizer<SyncPolicy> >(SyncPolicy(10), *image_subscriber_ptr_, *detection_sub_ptr__);
+        sync_ptr_->registerCallback(boost::bind(&VisionTracker::detectionCallback,this,_1, _2));
     }
 
     VisionTracker::~VisionTracker()
     {
 
+    }
+
+    void VisionTracker::detectionCallback(const sensor_msgs::ImageConstPtr& image_ptr,const vision_msgs::Detection2DArray::ConstPtr& detection_ptr)
+    {
+        return;
     }
 }
